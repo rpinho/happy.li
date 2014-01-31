9b31c6c99202404eff405f4411251300d39caee3
99fe34ab <99fe34ab@opayq.com>
2014-01-31 01:33:33 -0800

    before implementing new results.html page

--- myapp.py
+++ myapp.py
@@ -8,7 +8,7 @@ db = mdb.connect(user="root", host="localhost", port=3306, db="demo")
 cities_table = 'cities4'
 JOBS_TABLE = 'postings'#jobs_cities2'
 #weights = {'salary_f': 0.6, 'n1_f': 0.2, 'n2_f': 0.2}
-output = ['city', 'latitude', 'longitude', 'url', 'description']
+output = ['city', 'latitude', 'longitude', 'image_url', 'description']
 n_cities = 20
 
 @app.route("/")
@@ -45,7 +45,7 @@ def waypoints():
     for i in range(n_cities):
         cities.append({i: (df.ix[i, 'city'],
                            df.ix[i, 'latitude'], df.ix[i, 'longitude'],
-                           df.ix[i, 'url'], df.ix[i, 'description'])})
+                           df.ix[i, 'image_url'], df.ix[i, 'description'])})
     print cities
     return json.dumps(cities)
     #return cities.to_json(orient='split')#json.dumps(list(cities))#address)
@@ -53,13 +53,15 @@ def waypoints():
 def jobs(table=JOBS_TABLE):
     """Return list of projects."""
     query = 'select job from %s group by job' %table
-    df = read_query(query)
+    df = model.sql.frame_query(query, db)
     return json.dumps(df.job.values.tolist())
 
 # I have a dictionary that holds functions
 # that respond to json requests
 JSON = {
     'jobs': jobs,
+    'job1': jobs,
+    'job2': jobs
 }
 
 # jobs function is called here
