1064137b5df2950de97457103b18cf1947b3304b
99fe34ab <99fe34ab@opayq.com>
2014-02-17 19:26:32 -0800

    rm unused files

--- forms.py
+++
@@ -1,7 +0,0 @@
-from flask.ext.wtf import Form
-from wtforms import TextField, BooleanField, SelectField
-from wtforms.validators import Required
-
-class LoginForm(Form):
-    job1 = TextField('job1', validators = [Required()])
-    job2 = TextField('job2', validators = [Required()])
